64e9df459a5d299ef6aeabb03f7469672a9755fc
luymoon <aguiarluana780@gmail.com>
2024-06-13 11:38:32 -0300

    Lista de Tarefas 13-06

---
+++ myTaskList/src/app/home/index.js
@@ -0,0 +1,106 @@
+import React, { useState } from 'react'
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ImageBackground,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+  Alert
+} from 'react-native'
+import { Ionicons } from "@expo/vector-icons";
+import { styles } from './style';
+import ItemList from '../../components/ItemList';
+import { StatusBar } from 'expo-status-bar';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export default function Home() {
+  const [textInput, setTextInput] = useState('');
+  const [items, setItems] = useState([]);
+
+  const saveItemToDevice = async () => {
+
+  }
+
+  const getItemsFromDevice = async () => {
+
+  }
+
+  const addItem = () => {
+
+  }
+
+  const markItemBought = itemId => {
+
+  }
+
+  const unmarkItemBought = itemId => {
+
+  }
+
+  const removeItem = itemId => {
+
+  }
+
+  const removeAll = () => {
+    Alert.alert(
+      "Limpar Lista?", 
+      "Confirma a exclusão de todas as tarefas de sua lista?",
+      [{
+        text: 'Sim',
+        onPress: () => { setItems([]) }
+      }, {
+        text: 'Cancelar',
+        style: 'cancel',
+      }]
+    )
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <ImageBackground
+        source={require('../../assets/background.jpg')}
+        resizeMode='repeat'
+        style={{ flex: 1, justifyContent: 'flex-start'}}
+      >
+        <View style={styles.header}>
+          <Text style={styles.title}>Lista de Produtos</Text>
+          <View>
+            <Ionicons name="trash" size={32} color="#fff" onPress={removeAll} />
+          </View>
+        </View>
+
+        {/* Lista de Produtos */}
+        <FlatList
+          contentContainerStyle={{ padding: 20, paddingBottom: 100, color: '#fff' }}
+          data={items}
+          renderItem={({item}) =>
+            <ItemList
+              item={item}
+              markItem={markItemBought}
+              unmarkItem={unmarkItemBought}
+              removeItem={removeItem}
+            />
+          }
+        />
+
+        <View style={styles.footer}>
+          <View style={styles.inputContainer}>
+            <TextInput
+              color="#fff"
+              fontSize={18}
+              placeholderTextColor="#aeaeae"
+              placeholder="Digite a tarefa..."
+              value={textInput}
+              onChange={(text) => setTextInput(text)}
+            />
+          </View>
+          <TouchableOpacity style={styles.iconContainer} onPress={addItem}>
+            <Ionicons name="add" size={36} color="#fff" />
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>      
+    </SafeAreaView>
+  )
+}
